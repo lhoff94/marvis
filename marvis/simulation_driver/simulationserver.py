299ba742d8174da5bf60e1a3ac8dcb4fac6bbe23
Lukas Hoffmann <mail-github@lhoff.de>
2022-07-02 22:34:08 +0200

    changed endpoints to reflect current mpy-client implementation

--- marvis/simulation_driver/simulationserver.py
+++ marvis/simulation_driver/simulationserver.py
@@ -9,18 +9,33 @@ class SimulationServer:
         
         self.config = uvicorn.Config("marvis.simulation_driver.simulationserver:app", host="0.0.0.0", port=port, log_level="debug")
         
+        test_values = {
+            "Luminance":119,
+            "CO2":424,
+            "Temperature": 28,
+            "Pressure":1000009
+        }
 
         @app.get("/")
         async def root():
             return {"message": "Hello World"}
 
-        @app.get("/temperature/{place_id}")
-        async def read_temperature(place_id:str):
-            temperature = {
-                "berlin": 28,
-                "hamburg": 21
-            }
-            return {"temperature": temperature[place_id]}
+        @app.get("/sensor-1/Luminance")
+        async def read_luminance():
+            return test_values["Luminance"]
+        
+        @app.get("/sensor-1/CO2")
+        async def read_luminance():
+            return test_values["CO2"]
+
+        @app.get("/sensor-1/Temperature")
+        async def read_luminance():
+            return test_values["Temperature"]
+
+        @app.get("/sensor-1/Pressure")
+        async def read_luminance():
+            return test_values["Pressure"]
+
 
     
     def run(self):
