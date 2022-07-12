46f182ab2234f0744b4df811eaaaf4c8db9ca435
Lukas Hoffmann <mail-github@lhoff.de>
2022-07-12 17:46:44 +0200

    fix issues with logging

--- marvis/__init__.py
+++ marvis/__init__.py
@@ -2,7 +2,7 @@
 
 from .channel import Channel, CSMAChannel, WiFiChannel
 from .network import Network
-from .node import Node, SwitchNode, DockerNode, LXDNode, ExternalNode, SSHNode, InterfaceNode
+from .node import Node, SwitchNode, DockerNode, LXDNode, ExternalNode, SSHNode, InterfaceNode, ServiceNode
 from .scenario import Scenario
 from .argparse import ArgumentParser
 from .simulation_driver import SimulationServer
