2ed83df11934d62d501313197174382ed5cf1b4b
Lukas Hoffmann <mail-github@lhoff.de>
2022-09-13 17:43:14 +0200

    Final simserver

--- marvis/simulation_driver/simulationserver.py
+++ marvis/simulation_driver/simulationserver.py
@@ -9,38 +9,14 @@ class SimulationServer:
     def __init__(self, port):
         
         self.starttime = None
+        self.app = app
 
         self.config = uvicorn.Config("marvis.simulation_driver.simulationserver:app", host="0.0.0.0", port=port, log_level="debug")
         
-        test_values = {
-            "Luminance":119,
-            "CO2":424,
-            "Temperature": 28,
-            "Pressure":1000009
-        }
-
         @app.get("/")
         async def root():
-            return {"message": "Hello World"}
+            return {"message": "Hello Marvis"}
 
-        @app.get("/sensor-1/Luminance")
-        async def read_luminance():
-            return test_values["Luminance"]
-        
-        @app.get("/sensor-1/CO2")
-        async def read_luminance():
-            return test_values["CO2"]
-
-        @app.get("/sensor-1/Temperature")
-        async def read_luminance():
-            return test_values["Temperature"]
-
-        @app.get("/sensor-1/Pressure")
-        async def read_luminance():
-            return test_values["Pressure"]
-
-
-    
     def run(self):
         server = uvicorn.Server(self.config)
         self.starttime = time.time()
